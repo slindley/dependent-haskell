4751dd0b039d5a30fc6634103b41b317e8f310c0
sam <Sam.Lindley@ed.ac.uk>
2013-04-29 15:52:59 +0100

    tweaks

--- Box/Box.hs
+++ Box/Box.hs
@@ -28,6 +28,7 @@ natter :: Natty x -> (NATTY x => t) -> t
 natter Zy     t = t
 natter (Sy x) t = natter x t
 
+{- plus -}
 type family (m :: Nat) :+ (n :: Nat) :: Nat
 type instance Z :+ n = n
 type instance S m :+ n = S (m :+ n)
@@ -36,20 +37,41 @@ type instance S m :+ n = S (m :+ n)
 Zy /+/ n = n
 Sy m /+/ n = Sy (m /+/ n)
 
+{- minus -}
+type family (m :: Nat) :- (n :: Nat) :: Nat
+type instance Z   :- n   = Z
+type instance S m :- Z   = S m
+type instance S m :- S n = (m :- n)
+
+(/-/) :: Natty m -> Natty n -> Natty (m :- n)
+Zy   /-/ n    = Zy
+Sy m /-/ Zy   = Sy m
+Sy m /-/ Sy n = m /-/ n
+
+{- max -}
+type family Max (m :: Nat) (n :: Nat) :: Nat
+type instance Max Z     n     = n
+type instance Max (S m) Z     = S m
+type instance Max (S m) (S n) = S (Max m n)
+
+maxn :: Natty m -> Natty n -> Natty (Max m n)
+maxn Zy     n      = n
+maxn (Sy m) Zy     = Sy m
+maxn (Sy m) (Sy n) = Sy (maxn m n)
+
+
 data Cmp :: Nat -> Nat -> * where
-  LTNat :: ((Max x (x :+ S y) ~ (x :+ S y))) => Natty y -> Cmp x (x :+ S y)
-  EQNat :: (Max x x ~ x) => Cmp x x
-  GTNat :: ((Max (y :+ S x) y ~ (y :+ S x)),
-            (((y :+ S x) :- y) ~ S x)) =>
-             Natty x -> Cmp (y :+ S x) y
+  LTNat :: ((x :+ S z) ~ y,          Max x y ~ y, (x :- y) ~ Z)   => Natty z -> Cmp x y
+  EQNat :: (x          ~ y,          Max x y ~ x, (x :- y) ~ Z)   =>            Cmp x y
+  GTNat :: (x          ~ (y :+ S z), Max x y ~ x, (x :- y) ~ S z) => Natty z -> Cmp x y
 
 cmp :: Natty x -> Natty y -> Cmp x y
-cmp Zy Zy = EQNat
-cmp Zy (Sy y) = LTNat y
-cmp (Sy x) Zy = GTNat x
+cmp Zy     Zy     = EQNat
+cmp Zy     (Sy y) = LTNat y
+cmp (Sy x) Zy     = GTNat x
 cmp (Sy x) (Sy y) = case cmp x y of
   LTNat z -> LTNat z
-  EQNat -> EQNat
+  EQNat   -> EQNat
   GTNat z -> GTNat z
 
 data CmpCuts :: Nat -> Nat -> Nat -> Nat -> * where
@@ -63,7 +85,7 @@ cmpCuts Zy b (Sy c) d  = LTCuts c
 cmpCuts (Sy a) b Zy d  = GTCuts a
 cmpCuts (Sy a) b (Sy c) d = case cmpCuts a b c d of
   LTCuts z -> LTCuts z
-  EQCuts -> EQCuts
+  EQCuts   -> EQCuts
   GTCuts z -> GTCuts z
 
 {-
@@ -99,7 +121,7 @@ instance Cut p => Cut (Box p) where
   horCut xl xr Clear = (Clear, Clear)
   horCut xl xr (Hor x1 b1 x2 b2) = case cmpCuts xl xr x1 x2 of
     LTCuts z -> let (ll, lr) = horCut xl (Sy z) b1 in (ll, Hor (Sy z) lr x2 b2)
-    EQCuts -> (b1, b2)
+    EQCuts   -> (b1, b2)
     GTCuts z -> let (rl, rr) = horCut (Sy z) xr b2 in (Hor x1 b1 (Sy z) rl, rr)
   horCut xl xr (Ver y1 tb y2 bb) = (Ver y1 tl y2 bl, Ver y1 tr y2 br)
     where (tl, tr) = horCut xl xr tb ; (bl, br) = horCut xl xr bb
@@ -108,7 +130,7 @@ instance Cut p => Cut (Box p) where
   verCut yl yr Clear = (Clear, Clear)
   verCut yl yr (Ver y1 b1 y2 b2) = case cmpCuts yl yr y1 y2 of
     LTCuts z -> let (tt, tb) = verCut yl (Sy z) b1 in (tt, Ver (Sy z) tb y2 b2)
-    EQCuts -> (b1, b2)
+    EQCuts   -> (b1, b2)
     GTCuts z -> let (bt, bb) = verCut (Sy z) yr b2 in (Ver y1 b1 (Sy z) bt, bb)
   verCut yl yr (Hor x1 tb x2 bb) = (Hor x1 tl x2 bl, Hor x1 tr x2 br)
     where (tl, tr) = verCut yl yr tb ; (bl, br) = verCut yl yr bb
@@ -193,15 +215,6 @@ vGap :: Natty y -> Box p '(Z, y)
 vGap y = Clear
 
 {- placing boxes -}
-type family Max (m :: Nat) (n :: Nat) :: Nat
-type instance Max Z     n     = n
-type instance Max (S m) Z     = S m
-type instance Max (S m) (S n) = S (Max m n)
-
-maxn :: Natty m -> Natty n -> Natty (Max m n)
-maxn Zy     n      = n
-maxn (Sy m) Zy     = Sy m
-maxn (Sy m) (Sy n) = Sy (maxn m n)
 
 {-
 --- lemmas about max ---
@@ -232,24 +245,18 @@ joinH :: (Natty x1, Natty y1) -> (Natty x2, Natty y2) ->
             Box p '(x1, y1) -> Box p '(x2, y2) -> Box p '(x1 :+ x2, Max y1 y2)
 joinH (x1, y1) (x2, y2) b1 b2 =
   case cmp y1 y2 of
-    EQNat ->
-       (Hor x1 b1 x2 b2)
-    LTNat n' ->
-      (Hor x1 (Ver y1 b1 (Sy n') (clear (x1, Sy n'))) x2 b2)
-    GTNat n' ->
-       (Hor x1 b1 x2 (Ver y2 b2 (Sy n') (clear (x2, Sy n'))))
+    LTNat n' -> Hor x1 (Ver y1 b1 (Sy n') (clear (x1, Sy n'))) x2 b2
+    EQNat    -> Hor x1 b1 x2 b2
+    GTNat n' -> Hor x1 b1 x2 (Ver y2 b2 (Sy n') (clear (x2, Sy n')))
 
 -- place boxes vertically
 joinV :: (Natty x1, Natty y1) -> (Natty x2, Natty y2) ->
             Box p '(x1, y1) -> Box p '(x2, y2) -> Box p '(Max x1 x2, y1 :+ y2)
 joinV (x1, y1) (x2, y2) b1 b2 =
   case cmp x1 x2 of
-    EQNat    ->
-       (Ver y1 b1 y2 b2)
-    LTNat n' ->
-      (Ver y1 (Hor x1 b1 (Sy n') (clear (Sy n', y1))) y2 b2)
-    GTNat n' ->
-       (Ver y1 b1 y2 (Hor x2 b2 (Sy n') (clear (Sy n', y2))))
+    LTNat n' -> Ver y1 (Hor x1 b1 (Sy n') (clear (Sy n', y1))) y2 b2
+    EQNat    -> Ver y1 b1 y2 b2
+    GTNat n' -> Ver y1 b1 y2 (Hor x2 b2 (Sy n') (clear (Sy n', y2)))
 
 {- cropping -}
 type Size w h = (Natty w, Natty h)
@@ -307,18 +314,6 @@ minn Zy     Zy     = Zy
 minn Zy     (Sy n) = Zy
 minn (Sy m) Zy     = Zy
 minn (Sy m) (Sy n) = Sy (minn m n)
-
-{- minus -}
-type family (m :: Nat) :- (n :: Nat) :: Nat
-type instance Z   :- n   = Z
-type instance S m :- Z   = S m
-type instance S m :- S n = (m :- n)
-
-(/-/) :: Natty m -> Natty n -> Natty (m :- n)
-Zy   /-/ n    = Zy
-Sy m /-/ Zy   = Sy m
-Sy m /-/ Sy n = m /-/ n
-
 
 
 {-- mess --}
