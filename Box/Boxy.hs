afe896e8649241742ddf15ee3208150f59c09545
sam <Sam.Lindley@ed.ac.uk>
2013-06-04 16:02:25 +0100

    perp

--- Box/Boxy.hs
+++ Box/Boxy.hs
@@ -21,6 +21,14 @@ data SDimension :: Dimension -> * where
   SHor :: SDimension Hor
   SVer :: SDimension Ver
 
+type family Perp (d :: Dimension) :: Dimension
+type instance Perp Hor = Ver
+type instance Perp Ver = Hor
+
+perp :: SDimension d -> SDimension (Perp d)
+perp SHor = SVer
+perp SVer = SHor
+
 {- composition -}
 -- Comp r d m
 --   describes how to compose in dimension d with
@@ -31,21 +39,13 @@ type instance Comp r Hor m = '(m, r)
 type instance Comp r Ver m = '(r, m) 
 
 {- projection -}
-type family Proj1 (d :: Dimension) (s :: (Nat, Nat)) :: Nat
-type instance Proj1 Hor '(w, h) = w
-type instance Proj1 Ver '(w, h) = h
+type family Proj (d :: Dimension) (s :: (Nat, Nat)) :: Nat
+type instance Proj Hor '(w, h) = w
+type instance Proj Ver '(w, h) = h
 
-type family Proj2 (d :: Dimension) (s :: (Nat, Nat)) :: Nat
-type instance Proj2 Hor '(w, h) = h
-type instance Proj2 Ver '(w, h) = w
-
-proj1 :: SDimension d -> Size w h -> Natty (Proj1 d '(w, h))
-proj1 SHor (w, h) = w
-proj1 SVer (w, h) = h
-
-proj2 :: SDimension d -> Size w h -> Natty (Proj2 d '(w, h))
-proj2 SHor (w, h) = h
-proj2 SVer (w, h) = w
+proj :: SDimension d -> Size w h -> Natty (Proj d '(w, h))
+proj SHor (w, h) = w
+proj SVer (w, h) = h
 
 data Box :: ((Nat, Nat) -> *) -> (Nat, Nat) -> * where
   Stuff :: p wh -> Box p wh
@@ -86,8 +86,8 @@ cutEq r d m n w1 b1 w2 b2 =
                   in (Jux r d w1 b1 (SS z) b21, b22)
 
 -- unequal dimensions
-cutNeq d d' m n h1 b1 h2 b2 =
-  (Jux m d' h1 b11 h2 b21, Jux n d' h1 b12 h2 b22)
+cutNeq d m n h1 b1 h2 b2 =
+  (Jux m (perp d) h1 b11 h2 b21, Jux n (perp d) h1 b12 h2 b22)
   where (b11, b12) = cut h1 d m n b1
         (b21, b22) = cut h2 d m n b2
 
@@ -98,26 +98,26 @@ instance Cut p => Cut (Box p) where
   cut r d m n (Jux _ d' w1 b1 w2 b2) =
     case (d, d') of
       (SHor, SHor) -> cutEq r SHor m n w1 b1 w2 b2
-      (SHor, SVer) -> cutNeq SHor SVer m n w1 b1 w2 b2
-      (SVer, SHor) -> cutNeq SVer SHor m n w1 b1 w2 b2
+      (SHor, SVer) -> cutNeq SHor m n w1 b1 w2 b2
+      (SVer, SHor) -> cutNeq SVer m n w1 b1 w2 b2
       (SVer, SVer) -> cutEq r SVer m n w1 b1 w2 b2
 
 {- placing boxes -}
 join' ::
-  (Comp h1 d w1 ~ Comp w1 d' h1,
-   Comp h1 d w2 ~ Comp w2 d' h1,
-   Comp h2 d w1 ~ Comp w1 d' h2,
-   Comp h2 d w2 ~ Comp w2 d' h2) =>
-     SDimension d -> SDimension d' ->
+  (Comp h1 d w1 ~ Comp w1 (Perp d) h1,
+   Comp h1 d w2 ~ Comp w2 (Perp d) h1,
+   Comp h2 d w1 ~ Comp w1 (Perp d) h2,
+   Comp h2 d w2 ~ Comp w2 (Perp d) h2) =>
+     SDimension d ->
        Natty w1 -> Natty h1 ->
          Natty w2 -> Natty h2 ->
            Box p (Comp h1 d w1) -> Box p (Comp h2 d w2) ->
              Box p (Comp (Max h1 h2) d (w1 :+ w2))
-join' d d' w1 h1 w2 h2 b1 b2 =
+join' d w1 h1 w2 h2 b1 b2 =
   case cmp h1 h2 of
-    LTNat n -> Jux (maxn h1 h2) d w1 (Jux w1 d' h1 b1 (SS n) Clear) w2 b2
+    LTNat n -> Jux (maxn h1 h2) d w1 (Jux w1 (perp d) h1 b1 (SS n) Clear) w2 b2
     EQNat   -> Jux (maxn h1 h2) d w1 b1 w2 b2
-    GTNat n -> Jux (maxn h1 h2) d w1 b1 w2 (Jux w2 d' h2 b2 (SS n) Clear)
+    GTNat n -> Jux (maxn h1 h2) d w1 b1 w2 (Jux w2 (perp d) h2 b2 (SS n) Clear)
 
 
 -- place boxes next to each other
@@ -128,8 +128,8 @@ joinD :: SDimension d ->
                  Box p (Comp (Max h1 h2) d (w1 :+ w2))
 joinD d (w1, h1) (w2, h2) b1 b2 =
   case d of
-    SHor -> join' SHor SVer w1 h1 w2 h2 b1 b2
-    SVer -> join' SVer SHor w1 h1 w2 h2 b1 b2
+    SHor -> join' SHor w1 h1 w2 h2 b1 b2
+    SVer -> join' SVer w1 h1 w2 h2 b1 b2
 
 {- cropping -}
 type Size w h = (Natty w, Natty h)
@@ -139,11 +139,11 @@ type Region x y w h = (Point x y, Size w h)
 clipD :: Cut p =>
            SDimension d ->
              Size w h -> Natty x ->
-               Box p (Comp (Proj2 d '(w, h)) d (Proj1 d '(w, h))) ->
-                 Box p (Comp (Proj2 d '(w, h)) d (Proj1 d '(w, h) :- x))
+               Box p (Comp (Proj (Perp d) '(w, h)) d (Proj d '(w, h))) ->
+                 Box p (Comp (Proj (Perp d) '(w, h)) d (Proj d '(w, h) :- x))
 clipD d s x b =
-  case cmp (proj1 d s) x of
-    GTNat z -> snd (cut (proj2 d s) d x (SS z) b)
+  case cmp (proj d s) x of
+    GTNat z -> snd (cut (proj (perp d) s) d x (SS z) b)
     _       -> Clear
 
 clip :: Cut p => Size w h -> Point x y -> Box p '(w, h) -> Box p '(w :- x, h :- y)
